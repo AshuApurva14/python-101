1229835673b248958ae09db8a0f3c4b262461429
Ashutosh Apurva <apooorva01@gmail.com>
2025-05-21 03:33:46 +0000

    add few exercise

---
+++ read_and_write_files/rw_file.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+file_path=Path('C:','Users','Ashutosh')
+file_path1=str(Path('C:','Users','Ashutosh'))
+print(file_path)
+print(file_path1)
+
---
+++ read_and_write_files/string_slice.py
@@ -0,0 +1,8 @@
+str1="Welcome to Python 101: Strings"
+#new_string = str1[0:7]  # Slicing the string from index 0 to 6
+str2='1 '+ str1[0:7] + ' ryan to tyler'  # Slicing the string from index 0 to 6
+print(str2.title())
+
+rev_str='Google is your friend'
+
+print(rev_str[::-1])  # Reversing the string using slicing
