937e01936e4d8c77aec163c8454f77cff31fd420
Ashutosh Apurva <apooorva01@gmail.com>
2025-05-25 01:22:08 +0000

    learn maths in python

---
+++ maths_in_python/problem1.py
@@ -0,0 +1,21 @@
+# Program to show Commutative Property of Addition
+# Take two numbers as input from the user
+
+# Show the results of the Commutative Property of Addition
+
+a = int(input("Enter your First Number:"))
+b = int(input("Enter your Second Number:"))
+
+# Commutative Property of Addition
+
+print(f"\nCommutative Property of Addition: {a + b} = {b + a}")
+
+# Check if the Commutative Property of Addition holds true
+print("Checking if the Commutative property of Addition holds true...\n")
+
+if a+ b == b +a:
+    print("The Commutative Property of Addition holds true.")
+else:
+    print('oops! Th Commutative property of Addition does not hold true.')
+
+
