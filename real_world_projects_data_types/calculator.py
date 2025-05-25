4e7297645b229cfdc21439d768d3af655fa1b974
Ashutosh Apurva <apooorva01@gmail.com>
2025-05-25 02:31:53 +0000

    refactor calculator.py

--- real_world_projects_data_types/calculator.py
+++ real_world_projects_data_types/calculator.py
@@ -2,18 +2,47 @@
 
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
-operator = input("Enter operator: ")
 
-if operator == "+":
-    print(num1 + num2)
-elif operator == "-":
-    print(num1 - num2)  
-elif operator == "*":
-    print(num1 * num2)
-elif operator == "/":
-    print(num1 / num2)
-else:
-    print("Invalid operator")
+operator = input("Choose the operation from the below list \n 1. Add(+) \n 2.Substract(-) \n 3.Multiply(*) \n 4.Divide (/) \n 5.Modulus(%) \n") 
 
-# Run the code and test it with different numbers and operators.
+operator = operator.strip()  # Remove any leading/trailing whitespace
+
+operator = operator.lower()  # Convert to lowercase for consistency
+
+def is_valid_operator(op):
+    if op in ["+","-", "*", "/", "%"]:
+        print("Valid operator")
+    else:
+        print("Invalid operator ... Please try again! Valid operators are +, -, *, /, %")
+        
+
+# Perform the operation based on the operator
+
+def perform_operation(num1, num2):
+    is_valid_operator(operator)
+    result = None  # Initialize result variable
+    if operator == "+":
+       result = print(num1 + num2)
+    elif operator == "-":
+        result = print(num1 - num2)
+    return result
+
+
+__main__ = "__main__"
+if __name__ == "__main__":
+    perform_operation(num1, num2)
+
+
+
+# elif operator == "-":
+#     print(num1 - num2)  
+# elif operator == "*":
+#     print(num1 * num2)
+# elif operator == "/":
+#     print(num1 / num2)
+# elif operator == "%":
+#     print(num1 % num2)
+# else:
+#     print("Invalid operator ... Please try again! Valid operators are +, -, *, /, %")
+
 
