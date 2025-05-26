dcf38381c9f6abe7426acacbacd978915879c0fc
Ashutosh Apurva <apooorva01@gmail.com>
2025-05-26 02:10:55 +0000

    understood lists

---
+++ lists_in_python/understand_list.py
@@ -0,0 +1,17 @@
+# Understanding lists in Python
+
+# Lists in Python are ordered collections of items that can be of any data type.
+list1 = [11, 12, 13, 14, 15]* 2
+
+print(list1[::1])
+
+
+# Lists can contain different data types, including strings, integers, and even other lists.
+
+
+list2 =['A','B','C','D']
+
+for index, j in enumerate(list2):
+    print(f"index: {index}, value: {j}")
+
+
