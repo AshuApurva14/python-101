03713688294174634d6480873d5f287911072c6a
Ashutosh Apurva <apooorva01@gmail.com>
2025-05-28 02:17:29 +0000

    define strings

---
+++ strings_projects/define_strings.py
@@ -0,0 +1,21 @@
+# Python allow to define strings in multiple ways
+
+# 1. Single quotes,
+# 2. double quotes, 
+# 3. triple single quotes, 
+# 4. triple double quotes.
+
+single_quotes_string = 'This is a string defined with single quotes.'
+
+double_quotes_string = "This is a string defined with double quotes."
+
+triple_single_quotes_string = ''' This is a string defined with triple single quotes. '''
+
+triple_single_quotes_string = """ This is a string defined with triple double quotes. """
+
+# Strigs can be comcatenated using the + opertor
+
+conacatenated_string = single_quotes_string + double_quotes_string
+print(conacatenated_string)
+
+
