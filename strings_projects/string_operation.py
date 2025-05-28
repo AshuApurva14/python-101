402356032092c3c28d02b147acf001e1cd0ec98d
Ashutosh Apurva <apooorva01@gmail.com>
2025-05-28 03:23:37 +0000

    diff types of string operations

---
+++ strings_projects/string_operation.py
@@ -0,0 +1,21 @@
+# String operations in Python are essential for manipulating and analyzing text data. 
+#  Here are some common string operations with examples:
+
+# 1. Concatenation (+) - Joining two or more strings together.
+string1 = "Hello,"
+string2 = "World!"
+concatenated_string = string1 + string2
+print("Concatenated String:", concatenated_string)
+
+# 2. Repetition (*) - Repeating a string multiple times.
+repeated_string = string1 * 3
+print(f"Repeated_String:\n {repeated_string}")
+
+# 3. Slicing - Extracting a substring from a string.
+sliced_string = concatenated_string[0:5]  # Extracts "Hello"
+print("Sliced String: ", sliced_string)
+
+# 4. Length (len()) - Getting the length of a string.
+length_of_string = len(concatenated_string)
+print("Length of Concatenated String:", length_of_string)
+
