cd06abca44e0b06bea71fa3f1b00382f525db3d6
Ashutosh Apurva <apooorva01@gmail.com>
2025-05-29 03:47:39 +0000

    split and join methods

--- split_and_join/file1.py
+++ split_and_join/file1.py
@@ -1,4 +1,8 @@
-
-csv = 'Eric,John,Michael,Anna'
-
-print(csv.split(','))
+csv = 'Eric,John,Michael,Terry,Graham:TerryG;Brian'
+#print(csv.split(';'))
+# print(','.join(csv.split(':')))
+# print(','.join(csv.split(';')).split(':'))
+# print(','.join(','.join(csv.split(';')).split(':')))
+# friends_list = [','.join(','.join(csv.split(';')).split(':'))]
+friends_list = csv.replace(':', ',').replace(';',',').split(',')
+print(friends_list)
