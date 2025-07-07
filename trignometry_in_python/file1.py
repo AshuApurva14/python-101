ed723b1059dbe35bd21164d5c59531b266231b66
Ashutosh Apurva <apooorva01@gmail.com>
2025-07-07 02:44:53 +0000

    feat: add trignometric fucntions

---
+++ trignometry_in_python/file1.py
@@ -0,0 +1,12 @@
+# Import all the trignometric functions
+
+from math import sin, cos, tan, pi
+
+# Now find out the area of Circle
+
+r = 8
+
+area_of_circle = 2 * pi * r**2
+
+
+print(f"Area of Square: {area_of_circle} ")
