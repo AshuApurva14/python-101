b5be56f142baa0c67ccdb724babb0ce16838b524
Ashutosh Apurva <apooorva01@gmail.com>
2025-07-14 04:13:34 +0000

    update:dir name updated

---
+++ variables_in_py/hello_python.py
@@ -0,0 +1,10 @@
+print("Hello Python")
+
+def ai(n):
+    print(n)
+
+ai(4)
+
+ai_one = "GPT3"
+ai_two = "GPT4"
+
---
+++ variables_in_py/variables_2.py
@@ -0,0 +1,15 @@
+# Declare three variables
+
+num = 12
+
+name = "John"
+
+is_true = True
+
+rate = 12.12
+
+# Print the variables
+# The sep parameter is used to separate the variables with a newline character.
+# The default value of the sep parameter is a space character.
+print(num, name, is_true, rate, sep="\n")
+
