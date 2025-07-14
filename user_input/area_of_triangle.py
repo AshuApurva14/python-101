47c876ad27a5c7e9b5010f7aa26b445f51db6a24
Ashutosh Apurva <apooorva01@gmail.com>
2025-07-14 03:26:04 +0000

    new:area of triangle file

---
+++ user_input/area_of_triangle.py
@@ -0,0 +1,13 @@
+# This script calculates  Area of Triangle 
+
+# Let's first define two variables which stores value of Base and Height.
+
+# Base = Base of the Triangle
+# Height = Heiight of the Triangle
+
+base=input("Base of the Triangle ")
+height=input("Height of the Triangle ")
+
+area_of_triangle=1/2 * float(base) * float(height)
+
+print(f'Area of Triangle is {area_of_triangle}')
--- user_input/user_input1.py
+++ user_input/user_input1.py
@@ -1,3 +1,6 @@
 name=input("What is your name? ")
+age=input("How old are you? ")
 
-print(name)
+
+print(f"Your name is {name} and you are {age} years old.")
+
